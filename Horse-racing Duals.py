5878fcd26073b5425a64856c8fb5e4518ca9db26
SpiralT <45799516+SpiralT@users.noreply.github.com>
2019-06-27 21:51:45 -0400

    Update Horse-racing Duals.py

--- Horse-racing Duals.py
+++ Horse-racing Duals.py
@@ -25,3 +25,15 @@ for i in range(0,len(horse)-1):
 # To debug: print("Debug messages...", file=sys.stderr)
 
 print(min_diff)
+
+
+#========================================================
+#Other 1:
+pi = sorted([int(input()) for i in range(int(input()))])
+print(min([j-i for i,j in zip(pi[:-1],pi[1:])]))
+
+
+#========================================================
+#Other 2:
+l = sorted([int(input()) for _ in range(int(input()))])
+print(min((l[i+1] - l[i] for i in range(len(l) - 1))))
